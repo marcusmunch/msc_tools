a42b4a36287ed0b4109204db1cfbab8774df2bf5
Marcus Munch <Marcus@MarcusMunch.dk>
2019-09-24 12:54:44 +0200

    Reworked tadm_area
    Now works with multiple sheets per function call. If "sheets" parameter is missing, assumes all sheets.

--- tadm.R
+++ tadm.R
@@ -9,31 +9,44 @@
 #'
 
 
-tadm_area <- function(path, sheet){
-  #' This function reads data from the given path and sheet. It sums all measurements
-  #' that are below 0 and returns the data in long form
-  
+tadm_area <- function(path, sheets){
+  #' This function reads data from the given path and sheet(s). It sums all measurements
+  #' that are below 0 and returns the data in long form. If no sheet(s) is entered,
+  #' all sheets will be read.
+
+  # Set default parameter
+  if(missing(sheets)){
+    sheets = 1:length(excel_sheets(path))
+    print(paste("Sheets changed to:", sheets))
+  }
+
+  stopifnot(typeof(sheets) %in% c("double", "integer"))
+
   require(tidyverse)
   require(readxl)
-  
-  message(paste("Loading TADM data from ", path))
-  stopifnot(typeof(sheet) %in% c("double", "integer"))
-  message(paste('Sheet:', excel_sheets(path)[sheet]))
 
-  # Load the rest of the data
-  data <- read_excel(path, sheet = sheet) %>%
-    tail(-5)
+  message(paste("Loading TADM data from ", path))
+  
+  data <- tibble()
+  for(sheet in sheets){
+    message(paste('Sheet:', excel_sheets(path)[sheet]))
+    
+    this_data <- read_excel(path, sheet = sheet) %>%
+      tail(-5) %>%
+      mutate(setup = colnames(.)[1]) %>%
+      select(1, setup, everything())
+    
+    data <- data %>% bind_rows(this_data)
+  }
   
   # Convert to long form
   data <- data %>%
-    gather(key = "well", value = "tadm", -1) %>%
+    gather(key = "well", value = "tadm", -2) %>%
     mutate_at("tadm", as.numeric) %>%
     # Sum positive values for each well
     filter(tadm < 0) %>%
-    group_by(well) %>%
-    summarise(area = -sum(tadm)) %>%
-    # Append setup from upper left field 
-    mutate(setup = colnames(data)[1])
+    group_by(well, setup) %>%
+    summarise(area = -sum(tadm))
   
   return(data)
 }
